486f2b0242eb4a218931a9980586229dcd3a26c8
tangcongyuan <tangcongyuan@foxmail.com>
2017-12-27 15:04:01 -0500

    Updated staticfiles

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -150,12 +150,12 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'staticfiles'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # Simplified static file serving.
