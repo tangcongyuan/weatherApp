625ece5ec15e74288b6ac9c6ce142d776464c5ed
tangcongyuan <tangcongyuan@foxmail.com>
2017-12-27 13:24:43 -0500

    Updated static_root

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "newsLetter/static"),]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Mail server related config ############################################
 
